22f06c66b637f6a16a51d1a7724420850bd0302a
ukkasha345 <ukkashameh@gmail.com>
2022-05-14 13:16:44 +0500

    project upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var aboutus = document.getElementsByClassName("aboutUS_menu");
+
+function MousleftAbtUS(){
+    aboutus.style.visibility = 'hidden';
+}
+
+function mouseEnterAbouUs(){
+    aboutus.style.visibility = "visible";
+}
